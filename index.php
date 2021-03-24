3814bbf0faaa5c66b09fca59fd1876344cc9dca6
Nazarii Ivaniuk <nivaniu@a-139160.ups.u-psud.fr>
2021-03-24 09:57:32 +0100

    adding db

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 function br(){
 echo "<br>";
 }
@@ -7,4 +8,15 @@ br();
 echo "git fetch origin";
 br();
 echo "git pull";
+
+$c = ocilogon(DB_LOGIN, DB_PWD, DB_PWD);
+$texte = "select nom, prix from produit";
+br();
+echo $texte;
+$ordre = ociparse($c, $texte);
+ociexecute($ordre);
+while (ocifetchinto($ordre, $ligne)){
+echo $ligne[0].",".$ligne[1];
+br();
+}
 ?>
